5edd17c44eada9bcbdf84257c85d5da0257cbe4d
Terrorbladezz <anirudhsriram1995@gmail.com>
2021-02-28 13:14:39 +0530

    remove loop

---
+++ g4g/LinkedLists/removeLoop.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <iostream>
+#include<map>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+
+    Node(int value)
+    {
+        data = value;
+        next = NULL;
+    }
+};
+
+Node* removeLoop(Node* head){
+    map<Node*, int> m;
+    Node* prevValue = NULL;
+    while(head){
+        if(m.count(head) > 0){
+            prevValue->next = NULL;
+            return ;
+        }
+        prevValue = head;
+        m[head] = 0;
+    }
+}
+
+int main(){
+    Node* head =  new Node(1);
+
+}
